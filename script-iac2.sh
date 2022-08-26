7184a33af04f9e0198d0eee0c698cfe8b76a20cf
Jorge Roberto Machado da Silva <jrms_jorgesilva@hotmail.com>
2022-08-26 11:21:34 -0300

    Update script-iac2.sh
    updatting instalation of wget in order to certify that wget will be available

--- script-iac2.sh
+++ script-iac2.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+# instalação inicial
 echo "Atualizando o servidor..."
 apt-get update
 apt-get upgrade -y
 apt-get install apache2 -y
 apt-get install unzip -y
+apt-get install wget -y
 
-
+# mensagem de espera
 echo "Baixando e copiando os arquivos da aplicação..."
 
+# script principal
 cd /tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 unzip main.zip
